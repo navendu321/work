5c9328859f9d83cad831051c99078db6d5e86b4b
navendu321 <gupta.navendu@gmail.com>
2016-09-25 19:53:58 +0530

    Excel manipulation. f2f api.

---
+++ excel/insert_excel.py
@@ -0,0 +1,109 @@
+from time import sleep
+import json
+import urllib
+import urllib2
+from openpyxl import load_workbook
+from openpyxl import Workbook
+import webbrowser
+
+def insert_excel():
+
+    file_path = '/home/navendu/Downloads/recipe_bank_v3_9.11.xlsx'
+    book = load_workbook(file_path)
+    sheet_name = 'recipe_bank'
+    sheet = book.get_sheet_by_name(sheet_name)
+
+    page_count = 1
+    page_max = 351
+    while page_count != page_max:
+
+        url ="http://food2fork.com/api/search"
+        query_args = {"key": "e1254d39d79328ef647ae25737895fe8",
+                      "page": page_count}
+
+        encoded_args = urllib.urlencode(query_args)
+        complete_url = url + "?" + encoded_args
+        resp = urllib2.urlopen(complete_url)
+        data = json.load(resp)
+
+        if len(data['recipes']) != 30:
+            print "Num records %s for page %s" % (len(data['recipes']), page_count)
+
+        for i in data['recipes']:
+            sheet.append([i.get("publisher", ""),
+                          i.get("f2f_url", ""),
+                          i.get("title", ""),
+                          i.get("source_url", ""),
+                          i.get("recipe_id", ""),
+                          i.get("image_url", ""),
+                          i.get("social_rank", ""),
+                          i.get("publisher_url", "")])
+
+        page_count += 1
+        sleep(1)
+
+    book.save(file_path)
+
+    
+
+
+    """ NEW EXCEL FILE
+    # Create an new Excel file and add a worksheet.
+    workbook = xlsxwriter.Workbook('demo.xlsx')
+    worksheet = workbook.add_worksheet()
+
+    # Widen the first column to make the text clearer.
+    worksheet.set_column('A:A', 30)
+    worksheet.set_column('B:B', 30)
+    worksheet.set_column('C:C', 30)
+    worksheet.set_column('D:D', 30)
+    worksheet.set_column('E:E', 30)
+    worksheet.set_column('F:F', 30)
+    worksheet.set_column('G:G', 30)
+    worksheet.set_column('H:H', 30)
+
+
+    worksheet.write(0, 0, "PUBLISHER")
+    worksheet.write(0, 1, "F2F URL")
+    worksheet.write(0, 2, "TITLE")
+    worksheet.write(0, 3, "SOURCE URL")
+    worksheet.write(0, 4, "RECIPE ID")
+    worksheet.write(0, 5, "IMAGE URL")
+    worksheet.write(0, 6, "SOCIAL RANK")
+    worksheet.write(0, 7, "PUBLISHER URL")
+
+    workbook.close()
+    """
+
+
+def insert_ingredient_count():
+    file_path = '/home/navendu/Downloads/all_records.xlsx'
+    book = load_workbook(file_path)
+    sheet_name = 'recipe_bank'
+    sheet = book.get_sheet_by_name(sheet_name)
+
+    row = 1
+    for i in sheet.columns[1]:
+
+        if sheet.cell(row=row, column=11).value:
+            row += 1
+            continue
+
+        print row
+        url = i.value
+        if 'food2fork' in url:
+            resp = urllib2.urlopen(url)
+            count = resp.read().count('itemprop="ingredients"')
+            sheet.cell(row=row, column=11).value = count
+            row += 1
+            sleep(1)
+
+            if row % 100 == 0:
+                book.save(file_path)
+
+    book.save(file_path)
+
+
+if __name__ == "__main__":
+    #insert_excel()
+    insert_ingredient_count()
